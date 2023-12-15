38b4e984e22e2a454d891b1b6dbeb377bf67fe24
Alanasking <103567176+Alanasking@users.noreply.github.com>
2023-12-15 10:35:07 +0800

    change iptables mode for redhat8.4
    change iptables mode for redhat8.4

--- cmd/static-egressip-controller/main.go
+++ cmd/static-egressip-controller/main.go
@@ -18,6 +18,9 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/golang/glog"
@@ -42,6 +45,29 @@ func main() {
 	flag.Parse()
 	defer glog.Flush()
 
+	histIptablesMode := os.Getenv("IPTABLES_MODE")
+	if hostIptablesMode == "nf_tables" {
+		//chang iptables mode to nf_tables when os type is redhat8.4
+		changeIptablesMode()
+	}
+	minRouteTableId := 30000
+	minIdStr := os.Getenv("MIN_ROUTE_TABLES_ID")
+	if minIdStr != "" [
+		minRouteTableId, _ = strconv.Atoi(minIdStr)
+	} else {
+		os.Setenv("MIN_ROUTE_TABLES_ID", strconv.Itoa(minRouteTableId))
+	}
+	maxRouteTableId := 32765
+	maxIdStr := os.Getenv("MAX_ROUTE_TABLES_ID")
+	if maxIdStr != "" [
+		maxRouteTableId, _ = strconv.Atoi(maxIdStr)
+	} else {
+		os.Setenv("MAX_ROUTE_TABLES_ID", strconv.Itoa(maxRouteTableId))
+	}
+	if maxRouteTableId > 32765 || minRouteTableId< 30000 {
+		glog.Fatalf("Error out of rang: %v-%v", maxRouteTableId,minRouteTableId)
+        }
+		
 	glog.Infof("Running Nirmata static egress ip controller version: " + version.Version)
 	// set up signals so we handle the first shutdown signal gracefully
 	stopCh := signals.SetupSignalHandler()
@@ -75,6 +101,14 @@ func main() {
 
 	if err = controller.Run(2, stopCh); err != nil {
 		glog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
+
+func changeIptablesMode() {
+	glog.Infof("chang iptables mode to nf_tabels")
+	err := exec.Command("sh","-c","cd /sbin; ls -sf xtables -nft-multi iptables-save; in -sf xtabels-nft-multi ip6tables; ls -sf xtabels-nft-multi ip6tables-save").Run()
+        if err != nill {
+		glog.Fatalf("Error ng iptables mode to nf_tabels")
 	}
 }
 
